5c380097d5b9275a1c5e68814dc1d7ceeac5604f
nikmen <menounosnikitas@gmail.com>
2025-03-25 19:56:23 +0200

    Refactor ParagraphCheck methods to use instance variables and improve input handling

--- Coding/Programming_lang/lab_02/lab_02.py
+++ Coding/Programming_lang/lab_02/lab_02.py
@@ -2,44 +2,32 @@ import re
 
 class ParagraphCheck: 
     
-    @staticmethod
-    def simpleCheck(text):
+ 
+    def simpleCheck(self):
         sentence_pattern = r'([A-Z][a-z,\s]+[.!?])'    
-        sentences = re.findall(sentence_pattern, text)
-        return " ".join(sentences) == text
+        sentences = re.findall(sentence_pattern, self.text)
+        return " ".join(sentences) == self.text
 
-    @staticmethod
-    def checkWithNumbers(text): 
+    def checkWithNumbers(self): 
         sentence_pattern = r'([A-Z0-9][a-z0-9,\s]+[.!?])'  
-        sentences = re.findall(sentence_pattern, text)
-        return " ".join(sentences) == text
+        sentences = re.findall(sentence_pattern, self.text)
+        return " ".join(sentences) == self. text
 
-    @staticmethod
-    def checkWithGreek(text): 
+    def checkWithGreek(self): 
         sentence_pattern = r'([A-Z\u0386-\u03AB\u0391-\u03A9][a-z\u03AC-\u03CE\u03B1-\u03C9,\s]+[.!?;])'
-        sentences = re.findall(sentence_pattern, text)
-        return " ".join(sentences) == text
+        sentences = re.findall(sentence_pattern, self.text)
+        return " ".join(sentences) == self.text
 
 
 
 if __name__ == "__main__":
 
-    paragraph = input("Εισάγετε μια παράγραφο: ")
-    if ParagraphCheck.simpleCheck(paragraph):
-        print("Η παράγραφος είναι σωστή.")
-    else:
-        print("Η παράγραφος δεν είναι σωστή.")
-        
+    paragraph = ParagraphCheck(); 
+    paragraph.text = input("Enter a simple paragraph: ")
+    print(paragraph.simpleCheck())
     
-    paragraph = input("Εισάγετε μια παράγραφο: ")
-    if ParagraphCheck.checkWithNumbers(paragraph):
-        print("Η παράγραφος είναι σωστή.")
-    else:
-        print("Η παράγραφος δεν είναι σωστή.")
-        
-        
-    paragraph = input("Εισάγετε μια παράγραφο: ")
-    if ParagraphCheck.checkWithGreek(paragraph): 
-        print("Η παράγραφος είναι σωστή.")  
-    else:
-        print("Η παράγραφος δεν είναι σωστή.")
+    paragraph.text = input("Enter a paragraph with numbers: ")
+    print(paragraph.checkWithNumbers())
+    
+    paragraph.text = input("Enter a paragraph with greek: ")
+    print(paragraph.checkWithGreek())
